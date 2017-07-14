702d2dc284e69c022c816456a37cb0ea3fbf83b3
Colin Heyson <cheyson@gmail.com>
2017-07-14 11:27:38 -0400

    Add png output and update datetime
    Add the output to save the file and fix the data/time stuff.

--- Plot1.R
+++ Plot1.R
@@ -1,20 +1,18 @@
 
 library(data.table)
 library(dplyr)
-dataset <- fread('C:/Users/Colin/Documents/exdata_data_household_power_consumption/household_power_consumption.txt')
+dataset <- fread('C:/Users/Colin/Documents/exdata_data_household_power_consumption/household_power_consumption.txt',
+                na.strings = c('?'))
+# Combine data & time fields into one properly formatted POSIXct datetime field
+dataset$datetime <- as.POSIXct(paste(dataset$Date, dataset$Time), format = '%d/%m/%Y %H:%M:%S')
 dataset$Date <- as.Date(dataset$Date, '%d/%m/%Y')
 dataset <- subset(dataset, Date >= "2007-02-01" & Date <= "2007-02-02")
-dataset$Time <- strptime(dataset$Time, format = "%H:%M:%S")
 
-## define a function to take blanks or emptys into nas
-empty_as_na <- function(x){
-    if("factor" %in% class(x)) x <- as.character(x) ## since ifelse wont work with factors
-    ifelse(x %in% c("", " ", "NA", '$', '?'), NA, x)
-}
+## Plot 1
+# Set-up the png output with 480 x 480 pixelation
+png('Plot1.png', width = 480, height = 480)
 
-## transform all columns
-dataset %>% mutate_each(funs(empty_as_na)) -> dataset
-
-#Plot 1
-hist(as.numeric(dataset$Global_active_power), col = 'red',
+hist(dataset$Global_active_power, col = 'red',
      main = 'Global Active Power', xlab = 'Global Active Power (kilowatts)')
+
+dev.off()
